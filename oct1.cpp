38584c57dff6cb1e3c29c7633df7f1838c82b103
Hari Krishna <sharikrishna562000@gmail.com>
2020-10-02 18:55:00 +0530

    oct1

---
+++ oct1.cpp
@@ -0,0 +1,22 @@
+class RecentCounter {
+public:
+    deque<int> q;
+    int count;
+    RecentCounter() {
+         count=0;
+    }
+    
+    int ping(int t) {
+        while((!q.empty())&&(q.front()<t-3000))
+            q.pop_front();
+        q.push_back(t);
+        count=q.size();
+        return count;
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
