b695adb57a7d4a8494683b06d65f91e80b13dfd6
Hari Krishna <sharikrishna562000@gmail.com>
2020-10-02 19:01:03 +0530

    oct2

---
+++ combinationsum-oct2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int> > fans;
+    void func(vector<int> candidates,vector<int>& v,int target,int prsum,int ind)
+    {
+        if(prsum==target)
+        {
+            fans.push_back(v);
+            return;
+        }
+        if((ind>=candidates.size())||(prsum>target))
+        {
+            return ;
+        }
+
+        if(prsum+candidates[ind]<=target)
+        {
+            v.push_back(candidates[ind]);
+            func(candidates,v,target,prsum+candidates[ind],ind);
+            v.pop_back();
+        }
+                func(candidates,v,target,prsum,ind+1);
+        return ;
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> v;
+        sort(candidates.begin(),candidates.end());
+        int prsum=0,ind=0;
+        func(candidates,v,target,prsum,ind);
+        return fans;
+    }
+};
