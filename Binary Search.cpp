65cf8cf522981fc2a0cd828522179b8eab7170a5
Hari Krishna <sharikrishna562000@gmail.com>
2020-10-08 12:46:39 +0530

    Add binarysearch.cpp

---
+++ Binary Search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int left=0,right=n-1;
+        while(left<=right)
+        {
+            int mid=(left+right)/2;
+            //cout<<mid<<" "<<nums[mid]<<endl;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                right=mid-1;
+            else
+                left=mid+1;
+            //cout<<left<<" "<<right<<endl;
+        }
+        return -1;
+    }
+};
