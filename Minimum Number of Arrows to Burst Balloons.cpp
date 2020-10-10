ac72fecb1180a42aafa1f70b556edc88b169e81e
Hari Krishna <sharikrishna562000@gmail.com>
2020-10-10 18:17:22 +0530

    Added min ballons to burst ballons.cpp

---
+++ Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(),points.end());
+        int n=points.size();
+        if(points.size()==0)
+             return 0;
+        int val=1;
+        int fval=points[0][1];
+        for(int i=1;i<n;i++)
+        {
+            if(points[i][0]>fval)
+            {
+                val++;
+                fval=points[i][1];
+            }
+            else
+            {
+                if(points[i][1]<fval)
+                    fval=points[i][1];
+            }
+        }
+        return val;
+    }
+};
