32e619b4d7b782f03e4f69d407d202f1d2d58cef
Hari Krishna <sharikrishna562000@gmail.com>
2020-10-12 14:08:55 +0530

    Add buddy strings and remove duplicate letters

---
+++ Buddy Strings.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool buddyStrings(string A, string B) {
+        int n=A.length(),m=B.length();
+        if(n!=m)
+         return false;
+        int l=0;
+        char f=' ',s=' ';
+        for(int i=0;i<n;i++)
+        {
+            if(A[i]!=B[i])
+            {
+                if(f==' ')
+                {
+                    f=A[i];
+                    s=B[i];
+                }
+                else
+                {
+                    if(!((f==B[i])&&(s==A[i])))
+                    {
+                       return false;
+                    }
+                }
+                l++;
+            }
+        }
+        if(l==2)
+            return true;
+        if(l==0)
+        {
+            set<char> s;
+            for(int i=0;i<n;i++)
+            {
+                if(s.find(A[i])!=s.end())
+                    return true;
+                s.insert(A[i]);
+            }
+        }
+          return false;
+    }
+};
