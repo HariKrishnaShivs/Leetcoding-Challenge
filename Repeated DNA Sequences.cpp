0c5999995c6ea13fb416382e4437b4b9a01ad2c7
Hari Krishna <sharikrishna562000@gmail.com>
2020-10-17 13:02:18 +0530

    Added Repeated DNA Sequences.cpp

---
+++ Repeated DNA Sequences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        map<string,int> mp;
+        int n=s.length();
+        vector<string> v;
+        if(n<=10)
+            return v;
+        string temp="";
+        for(int i=0;i<10;i++)
+        {
+            temp+=s[i];
+        }
+        mp.insert({temp,1});
+        for(int i=10;i<n;i++)
+        {
+            temp.erase(temp.begin());
+            temp+=s[i];
+            mp[temp]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.second>1)
+             v.push_back(it.first);
+        }
+        return v;
+    }
+};
